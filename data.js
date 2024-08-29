049b12e414963606318fd7c70a285403786aa7b2
Harsh Kumar Singh <93397540+harshuopjs@users.noreply.github.com>
2024-08-29 21:55:40 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,57 @@
+export const bio = [
+    "I am a passionate and dedicated web developer with a strong foundation in HTML,CSS,JS,JSX,JAVA,Python. I have a strong background in web development and have experience in creating responsive and user-friendly websites.",
+    "I am skilled in Cybersecurity and have experience in penetration testing and network security. It's been nearly a year since I started my journey in web development, and I am excited to continue my learning and growth as a Full stack Software Engineer.",
+];
+
+export const skills = [
+    {
+        title: "IDE",
+        skillName: "VS Code Insiders, IntelliJ IDEA, PyCharm",
+        color: "6",
+        percentage: "98",
+    },
+    {
+        title: "Design",
+        skillName: "HTML, Bootstrap, Tailwind",
+        color: "4",
+        percentage: "95",
+    },
+    {
+        title: "Version Control",
+        skillName: "GitHub, Bitbucket",
+        color: "7",
+        percentage: "85",
+    },
+    {
+        title: "Languages",
+        skillName: "JavaScript, TypeScript",
+        color: "1",
+        percentage: "79",
+    },
+    {
+        title: "Frameworks/Libraries",
+        skillName: "Angular, RxJS",
+        color: "6",
+        percentage: "72",
+    },
+    {
+        title: "Tools",
+        skillName: "Chrome DevTools",
+        color: "3",
+        percentage: "40",
+    },
+];
+
+export const projects = [""];
+
+export const experience = [
+    ""
+];
+
+export const education = [
+    "B.Tech CSE JIMS (G.G.S.I.P.U) GREATER NOIDA 2023-2027",
+    "11th - 12th G.S.B.V. Burari BURARI 2021-2023",
+    "KG-10Th JMPS BURARI,Nathupura 2019-2021",
+];
+
+export const footer = [""];
